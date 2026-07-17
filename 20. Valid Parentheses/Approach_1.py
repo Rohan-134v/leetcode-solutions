e3fb8bd7f6b1ef6794a8c54513b585eb8322f702
Rohan <160718905+Rohan-134v@users.noreply.github.com>
2026-07-17 11:50:04 +0530

    Easy: 20. Valid Parentheses | Approach 1 | Runtime: 0 ms | Memory: 19208000

---
+++ 20. Valid Parentheses/Approach_1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+            
+        stack = []
+        
+        for c in s:
+            if c == '(':
+                stack.append(')')
+            elif c == '{':
+                stack.append('}')
+            elif c == '[':
+                stack.append(']')
+            elif not stack or stack.pop() != c:
+                return False
+                
+        return not stack
