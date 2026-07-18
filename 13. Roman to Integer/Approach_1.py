b5bdb1389430140aa9445f23ee76ece5e79203c8
Rohan <160718905+Rohan-134v@users.noreply.github.com>
2026-07-18 22:22:40 +0530

    Easy: 13. Roman to Integer | Approach 1 | Runtime: 12 | Memory: 17796000

---
+++ 13. Roman to Integer/Approach_1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        
+        for i in range(len(s)):
+            current_value = d[s[i]]
+            if i + 1 < len(s) and d[s[i+1]] > current_value:
+                total -= current_value  
+            else:
+                total += current_value         
+        return total
