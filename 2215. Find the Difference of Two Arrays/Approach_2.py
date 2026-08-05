d0d750dc099f0ab455f29a3abe8946e9f0cd52ac
Rohan <160718905+Rohan-134v@users.noreply.github.com>
2026-08-05 11:15:53 +0530

    Easy: 2215. Find the Difference of Two Arrays | Approach 2 | Runtime: 9 | Memory: 19424000

--- 2215. Find the Difference of Two Arrays/Approach_2.py
+++ 2215. Find the Difference of Two Arrays/Approach_2.py
@@ -1,6 +1,9 @@
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        results = []
-        results.append(list(set([num for num in nums1 if num not in nums2])))
-        results.append(list(set([num for num in nums2 if num not in nums1])))
-        return results
+        s1 = set(nums1)
+        s2 = set(nums2)
+
+        d1 = list(s1 - s2)
+        d2 = list(s2 - s1)
+
+        return [d1, d2]
