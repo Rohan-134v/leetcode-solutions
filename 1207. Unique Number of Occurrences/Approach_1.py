cc18fadf458a77cf7802f09cde3482a53502cd8e
Rohan <160718905+Rohan-134v@users.noreply.github.com>
2026-08-05 11:22:15 +0530

    Easy: 1207. Unique Number of Occurrences | Approach 1 | Runtime: 11 | Memory: 19104000

---
+++ 1207. Unique Number of Occurrences/Approach_1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        h = {}
+        for i in range(len(arr)):
+            if arr[i] not in h:
+                h[arr[i]] = arr.count(arr[i])
+
+        return len(h) == len(set(h.values()))
+
+        
