5e751adcac314d62e19bddbee98940f609913929
Rohan <160718905+Rohan-134v@users.noreply.github.com>
2026-08-06 08:22:02 +0530

    Easy: 3345. Smallest Divisible Digit Product I | Approach 1 | Runtime: 2 | Memory: 19344000

---
+++ 3345. Smallest Divisible Digit Product I/Approach_1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def smallestNumber(self, n: int, t: int) -> int:
+        while True:
+            digit_product = 1
+            for digit in str(n):
+                digit_product *= int(digit)
+
+            if digit_product % t == 0 :
+                return n
+            else :
+                n += 1
+            
+        
+    
