fe506517cf84515782f5ba7f9aee0f46d88dee61
Rohan <160718905+Rohan-134v@users.noreply.github.com>
2026-08-22 22:01:25 +0530

    Hard: 23. Merge k Sorted Lists | Approach 1 | Runtime: 5 | Memory: 22872000

---
+++ 23. Merge k Sorted Lists/Approach_1.py
@@ -0,0 +1,29 @@
+import heapq
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeKLists(self, lists: list[Optional[ListNode]]) -> Optional[ListNode]:
+        heap = []
+
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, i, node))
+                
+        dummy = ListNode(0)
+        current = dummy
+
+        while heap:
+            val, index, node = heapq.heappop(heap)
+
+            current.next = node
+            current = current.next
+
+            if node.next:
+                heapq.heappush(heap, (node.next.val, index, node.next))
+                
+        return dummy.next
