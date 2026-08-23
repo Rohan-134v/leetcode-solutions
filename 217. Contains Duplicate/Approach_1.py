8f487591c794f30b5b175fd5ca7d2763238af19d
Rohan <160718905+Rohan-134v@users.noreply.github.com>
2026-08-23 10:06:46 +0530

    Easy: 217. Contains Duplicate | Approach 1 | Runtime: 37 | Memory: 37420000

---
+++ 217. Contains Duplicate/Approach_1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        count = Counter(nums)
+
+        for num in count.values():
+            if num >= 2:
+                return True
+        
+        return False
+        
