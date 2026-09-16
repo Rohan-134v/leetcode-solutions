75d8bb8a3203c5c7aa69bb0cb6e422f74966943c
Rohan <160718905+Rohan-134v@users.noreply.github.com>
2026-09-16 10:16:12 +0530

    Medium: 1621. Number of Sets of K Non-Overlapping Line Segments | Approach 2 | Runtime: 486 | Memory: 19520000

--- 1621. Number of Sets of K Non-Overlapping Line Segments/Approach_2.py
+++ 1621. Number of Sets of K Non-Overlapping Line Segments/Approach_2.py
@@ -1,6 +1,14 @@
-import math
-
 class Solution:
     def numberOfSets(self, n: int, k: int) -> int:
-        MOD = 10**9 + 7
-        return math.comb(n + k - 1, 2 * k) % MOD
+        mod = 10**9 + 7
+        dp = [1] * n
+        prefix_sums = [0] * (n + 1)
+        for j in range(n):
+            prefix_sums[j + 1] = (prefix_sums[j] + dp[j]) % mod
+        for _ in range(k):
+            dp[0] = 0
+            for j in range(1, n):
+                dp[j] = (dp[j - 1] + prefix_sums[j]) % mod
+            for j in range(n):
+                prefix_sums[j + 1] = (prefix_sums[j] + dp[j]) % mod
+        return dp[n - 1]
